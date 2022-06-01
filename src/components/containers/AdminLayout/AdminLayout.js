db926d8ecbc2809ac1ca20a1b31032f21c12c3d4
Kvazar3S273 <ba2h007@gmail.com>
2022-06-02 01:03:08 +0300

    edit admin panel

--- src/components/containers/AdminLayout/AdminLayout.js
+++ src/components/containers/AdminLayout/AdminLayout.js
@@ -3,21 +3,33 @@ import { Link, Outlet } from 'react-router-dom';
 import Header from "./Header";
 import "./style.css";
 import logo from "../../../../src/3x4logo.png";
+import user from "../../../../src/user.jpg";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faAd, faArrowRightFromBracket, faGear, faInfoCircle, faMoneyBillTrendUp, faTv, faUserCircle, faUsers } from '@fortawesome/free-solid-svg-icons'
+import { faAd, faArrowRightFromBracket, faCamera, faGear, faInfoCircle, faMoneyBillTrendUp, faSearch, faTv, faUserCircle, faUsers } from '@fortawesome/free-solid-svg-icons'
 
 const AdminLayout = () => {
+  function toggleMenu(){
+    let toggle = document.querySelector('.toggle');
+    let navigation = document.querySelector('.navigation');
+    let main = document.querySelector('.main');
+    toggle.classList.toggle('active');
+    navigation.classList.toggle('active');
+    main.classList.toggle('active');
+
+  }
     return (
       <>
         <main>
-      {/* <h1>AdminLayout</h1> */}
           {<Outlet />}
           <div className="adminContainer">
             <div className="navigation">
               <ul>
                 <li>
-                    <img className="logo" src={logo} alt="logo" width="100px" />
-                    <span className="title">Фотокопіцентр 3х4</span>
+                  <a>
+                    {/* <img className="logo" src={logo} alt="logo"/> */}
+                    <span className="icon" ><i style={{color:"#03a9f4", fontSize:"35px"}}><FontAwesomeIcon icon={faCamera} /></i></span>
+                    <span className="title" style={{color:"#03a9f4", fontSize:"20px"}}>3х4 Фотокопіцентр</span>
+                  </a>
                 </li>
                 <li>
                   <a>
@@ -69,6 +81,26 @@ const AdminLayout = () => {
                 </li>
               </ul>
             </div>
+
+            <div className="main">
+              <div className="topbar">
+                <div className="toggle" onClick={toggleMenu}></div>
+                <div className="search">
+                  <label>
+                    <input type="text" placeholder="Search here"/>
+                    <i><FontAwesomeIcon icon={faSearch}/></i>
+                  </label>
+                </div>
+                <div className="user">
+                    <img src={user} alt="user"/>
+                </div>
+
+              </div>
+
+            </div>
+
+
+
           </div>
         </main>
         </>
