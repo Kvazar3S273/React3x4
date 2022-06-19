962809401d9a06ffa50e4b9fe8058a17a92d2686
Kvazar3S273 <ba2h007@gmail.com>
2022-06-19 18:29:34 +0300

    edit conteiner producers

--- src/components/pages/home/producers.js
+++ src/components/pages/home/producers.js
@@ -4,37 +4,35 @@ import "./index.css";
 const Producers = () => {
   return (
     <div className="row">
-      <div className="col rounded py-3 mt-3" style={{backgroundColor:"#132855"}}>
-        <div className="row gx-4 justify-content-center">
-            {/* <img className="bg-img" src="/images/partners/partner_bg.png" alt="..."/> */}
-          <div className="container" 
-          // style={{position: "relative"}}
-          >
-                  <div 
-                  // style={{position: "absolute", top:"45%", left:"50%", width:"80%", transform: "translate(-50%, -50%)"}}
-                  >
-                    <h1 className="text-light text-center">Виробники</h1>
-                    <p className="text-light text-center">
-                      Ми користуємось витратними матеріалами тільки перевірених
-                      виробників <br/>щоб забезпечити високу якість нашої продукції
-                    </p>
-                
-                    <div className="row">
-                      <div className="col mx-4">
-                        <img className="img-partners" src="/images/partners/partner1.png" alt="..."/>
-                      </div>
-                      <div className="col mx-4">
-                        <img className="img-partners" src="/images/partners/partner2.png" alt="..."/>
-                      </div>
-                      <div className="col mx-4">
-                        <img className="img-partners" src="/images/partners/partner3.png" alt="..."/>
-                      </div>
-                      <div className="col mx-4">
-                        <img className="img-partners" src="/images/partners/partner4.png" alt="..."/>
-                      </div>
-                    </div>
+      <div
+        className="col rounded  mt-3"
+        style={{ backgroundColor: "#132855" }}
+      >
+        <div className="row justify-content-center">
+          <div className="container">
+            <div>
+              <h1 className="text-light text-center mt-2">Виробники</h1>
+              <p className="text-light text-center">
+                Ми користуємось витратними матеріалами тільки перевірених
+                виробників <br />
+                щоб забезпечити високу якість нашої продукції
+              </p>
 
-                  </div>
+              <div className="row justify-content-center mb-3">
+                <div className="col-10 col-sm-5 col-md-3 producer">
+                  <img className="img-partners" src="/images/partners/partner1.png" alt="Epson"/>
+                </div>
+                <div className="col-10 col-sm-5 col-md-3 producer">
+                  <img className="img-partners" src="/images/partners/partner2.png" alt="Videx"/>
+                </div>
+                <div className="col-10 col-sm-5 col-md-3 producer">
+                  <img className="img-partners" src="/images/partners/partner3.png" alt="WWM"/>
+                </div>
+                <div className="col-10 col-sm-5 col-md-3 producer">
+                  <img className="img-partners" src="/images/partners/partner4.png" alt="GP" />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
