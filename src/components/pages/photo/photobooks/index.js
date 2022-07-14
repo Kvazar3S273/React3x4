85214c8b87ed013474f58b51d662866fa2fe4f68
Kvazar3S273 <ba2h007@gmail.com>
2022-07-14 17:35:15 +0300

    edit photobooks

--- src/components/pages/photo/photobooks/index.js
+++ src/components/pages/photo/photobooks/index.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React from "react";
 
-const Photobooks = () => {
+
+const Photobooks = () => { 
+  
+  const handleClick = () => {
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+    script.async = true;
+    script.src = "https://pro.cyfrolab.com/widget/calc.js";
+    document.getElementsByTagName("head")[0].appendChild(script);
+  }
+
   return (
     <div className="row">
       <div className="offset-md-4 col-md-4">
         <h2 className="text-center mt-3">Фотокниги</h2>
+
+        <div
+          id="widget_calc_user"
+          data-user-key="9893e8659bc23351fd92d9c626c02df4"
+          data-user-lang="uk"
+          data-user-currency="UAH"
+        >
+          <button
+            type="submit"
+            className="btn btn-primary mt-4"
+            onClick={handleClick}
+          >
+            Калькулятор
+          </button>
+        </div>
       </div>
     </div>
   );
