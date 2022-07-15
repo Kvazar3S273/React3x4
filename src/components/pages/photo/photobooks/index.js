41b1d69a07f6a11c5700a894149d8e3b82784a60
Kvazar3S273 <ba2h007@gmail.com>
2022-07-15 13:40:28 +0300

    fix calculator

--- src/components/pages/photo/photobooks/index.js
+++ src/components/pages/photo/photobooks/index.js
@@ -27,50 +27,75 @@ const Photobooks = () => {
     duration: 2000,
   };
 
+
+  function showCalculator() {
+    const container = document.getElementById('widget_calc_user');
+
+    const button = document.getElementById("show_calc");
+    if (!button.visible) {
+      button.innerText = "Сховати калькулятор";
+      button.visible = true;
+      container.style.display = "block";
+    }
+    else
+    {
+      button.innerText = "Калькулятор";
+      button.visible = false;
+      container.style.display = "none";
+    }
+
+    if (button.created)
+      return;
+    button.created = true;
+
+    const id = container.dataset.userKey;
+    const domain = 'pro.cyfrolab.com';
+    const lang = container.dataset.userLang;
+    const langPrefix = lang === 'uk' ? '' : '/' + lang;
+    const currency = container.dataset.userCurrency;
+    const refererUrl = encodeURIComponent(document.location.href);
+  
+    const widget_url = `https://${domain}${langPrefix}/calculator?user_id=${id}&currency=${currency}&referer=${refererUrl}`;
+  
+    const Widget = {
+      created: false,
+      widgetElement: null,
+      show: function() {
+        if (this.created)
+          return;
+        this.widgetElement = document.createElement('div');
+        this.widgetElement.setAttribute('id', 'widget_container');
+        this.widgetElement.innerHTML = `<iframe id="widget_iframe" src="${widget_url}" frameborder="0" width="100%"></iframe>`;
+        const widget = document.getElementById('widget_calc_user');
+        widget.style.width = '100%';
+        widget.style.minHeight = '1000px';
+        widget.appendChild(this.widgetElement);
+        this.widgetElement.style.display = 'block';
+        this.created = true;
+        const widget_iframe = document.getElementById('widget_iframe');
+        widget_iframe.style.minHeight= '1000px';
+      }
+    }
+    Widget.show();
+  }
+
+
   return (
     <div className="row">
       <div className="offset-md-4 col-md-4">
         <h2 className="text-center mt-3">Фотокниги</h2>
-        
       </div>
-      <div className="row">
-          <div className="col-md-4">
-            <Zoom {...zoomInProperties}>
-              {images1.map((each, index) => (
-                <div key={index} style={{ width: "100%" }}>
-                  <img
-                    style={{ objectFit: "cover", width: "100%" }}
-                    src={each}
-                  />
-                </div>
-              ))}
-            </Zoom>
-          </div>
-          <div className="col-md-4">
-            <Zoom {...zoomInProperties}>
-              {images2.map((each, index) => (
-                <div key={index} style={{ width: "100%" }}>
-                  <img
-                    style={{ objectFit: "cover", width: "100%" }}
-                    src={each}
-                  />
-                </div>
-              ))}
-            </Zoom>
-          </div>
-          <div className="col-md-4">
-            <Zoom {...zoomInProperties}>
-              {images3.map((each, index) => (
-                <div key={index} style={{ width: "100%" }}>
-                  <img
-                    style={{ objectFit: "cover", width: "100%" }}
-                    src={each}
-                  />
-                </div>
-              ))}
-            </Zoom>
-          </div>
+
+      <div>
+      <button onClick={showCalculator} id="show_calc">Калькулятор</button>
+        <div
+          id="widget_calc_user"
+          data-user-key="9893e8659bc23351fd92d9c626c02df4"
+          data-user-lang="uk"
+          data-user-currency="UAH"
+        >
         </div>
+      </div>
     </div>
   );
 };
