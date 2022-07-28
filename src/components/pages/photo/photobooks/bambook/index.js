7d557a2a6c9efd00dc2ac3842ca70851ce3774b1
Kvazar3S273 <ba2h007@gmail.com>
2022-07-28 23:03:45 +0300

    edit text for slimbook

--- src/components/pages/photo/photobooks/bambook/index.js
+++ src/components/pages/photo/photobooks/bambook/index.js
@@ -184,6 +184,7 @@ const BamBook = () => {
               </p>
             </div>
           </div>
+          
         </div>
 
         <div
--- src/components/pages/photo/photobooks/slimbook/index.js
+++ src/components/pages/photo/photobooks/slimbook/index.js
@@ -14,22 +14,43 @@ const SlimBook = () => {
   //https://www.npmjs.com/package/react-slideshow-image
 
   const img_slider1 = [
-    "images/services/photo/photobooks/BamBook/1.jpg",
-    "images/services/photo/photobooks/BamBook/2.jpg",
+    "images/services/photo/photobooks/SlimBook/1.jpg",
+    "images/services/photo/photobooks/SlimBook/2.jpg",
+    "images/services/photo/photobooks/SlimBook/3.jpg",
   ];
   const img_slider2 = [
-    "images/services/photo/photobooks/BamBook/3.jpg",
-    "images/services/photo/photobooks/BamBook/4.jpg",
+    "images/services/photo/photobooks/SlimBook/4.jpg",
+    "images/services/photo/photobooks/SlimBook/5.jpg",
+  ];
+  const img_slider3 = [
+    "images/services/photo/photobooks/SlimBook/6.jpg",
+    "images/services/photo/photobooks/SlimBook/7.jpg",
   ];
   const img_slider4 = [
-    "images/services/photo/photobooks/BamBook/6.jpg",
-    "images/services/photo/photobooks/BamBook/7.jpg",
+    "images/services/photo/photobooks/SlimBook/8.jpg",
+    "images/services/photo/photobooks/SlimBook/9.jpg",
   ];
   const img_slider5 = [
-    "images/services/photo/photobooks/BamBook/8.jpg",
-    "images/services/photo/photobooks/BamBook/9.jpg",
-    "images/services/photo/photobooks/BamBook/10.jpg",
-    "images/services/photo/photobooks/BamBook/11.jpg",
+    "images/services/photo/photobooks/SlimBook/10.jpg",
+    "images/services/photo/photobooks/SlimBook/11.jpg",
+    "images/services/photo/photobooks/SlimBook/12.jpg",
+    "images/services/photo/photobooks/SlimBook/13.jpg",
+    "images/services/photo/photobooks/SlimBook/14.jpg",
+    "images/services/photo/photobooks/SlimBook/15.jpg",
+    "images/services/photo/photobooks/SlimBook/16.jpg",
+  ];
+  const img_slider6 = [
+    "images/services/photo/photobooks/SlimBook/17.jpg",
+    "images/services/photo/photobooks/SlimBook/18.jpg",
+    "images/services/photo/photobooks/SlimBook/19.jpg",
+    "images/services/photo/photobooks/SlimBook/20.jpg",
+    "images/services/photo/photobooks/SlimBook/21.jpg",
+  ];
+  const img_slider7 = [
+    "images/services/photo/photobooks/SlimBook/22.jpg",
+    "images/services/photo/photobooks/SlimBook/23.jpg",
+    "images/services/photo/photobooks/SlimBook/24.jpg",
+    "images/services/photo/photobooks/SlimBook/25.jpg",
   ];
 
   const zoomInProperties = {
@@ -61,15 +82,14 @@ const SlimBook = () => {
             <div className="col-sm-12 col-md-5 px-3">
               <h3 className="text-center">Про SlimBook</h3>
               <p>
-                Фотокнига Bambook - це поліграфічна фотокнига з щільними
-                крейдованими листами і розкриттям блоку на 180°. У новому БамБук
-                Ілюмінат використана сучасна технологія покриття розворотів
-                спеціалізованною цифровою плівкою, яка підсилює кольоропередачу,
-                додає кольору глибини, а завдяки блискучій поверхні розвороти
-                схожі на справжній глянцевий журнал. Для склейки сторінок
-                використовується термоклеевий прошарок зі спеціального складу.
-                Технологія перевірена роками і гарантує надійне та рівномірне
-                склеювання листів.
+                Фотокнига slimbook - це ідеальне поєднання поліграфії та 
+                фотодруку. Дана фотокнига поєднує у собі якість фотокниги 
+                та доступність поліграфічних технологій. Slimbook виготовлений 
+                за найбільш передовою технологією виробництва фотокниг, в основі
+                якої лежить використання спеціального прошарку між сторінками,
+                а не пластика чи картону. В результаті цього, сторінки, 
+                надруковані на фотопапері Fujicolor Crystal Archive LUSTRE,
+                виходять щільними, гнучкими та не піддаються деформаціям.
               </p>
             </div>
           </div>
@@ -79,14 +99,15 @@ const SlimBook = () => {
             <div className="col-sm-12 col-md-5 px-3">
               <h3 className="text-center">Основа</h3>
               <p>
-                Основа між розворотами — ідентична фотокнигам slimbook. В
-                bambook не використовуються пластикові чи картонні прошарки між
-                розворотами сторінок, завдяки чому книжковий блок виглядає
-                достатньо цупким та гнучким одночасно. В той самий час,
-                спеціальний склад між сторінками гарантує відсутність деформацій
-                розворотів при правильних умовах зберігання фотокниг. В
-                фотокнигах bambook Ви маєте змогу створювати макети обсягами до
-                40 розворотів.
+                Основа між розворотами — головна «родзинка» Слімбука. Завдяки 
+                їй він легкий, майже як поліграфічний printbook, і водночас
+                має якість зображення класичного photobook. У Slimbook не
+                використовуються пластикові та картонні прошарки між розворотами 
+                сторінок, завдяки чому блок стає тоншим. У той самий час спеціальний 
+                склад між сторінками гарантує відсутність деформацій розворотів.
+                За рахунок зменшення товщини сторінок, технологія slimbook 
+                дозволяє створювати макети книг об’ємом до 40-ка розворотів 
+                з розкриттям блоку на 180°!
               </p>
             </div>
             <div className="col-sm-12 col-md-7">
@@ -106,41 +127,38 @@ const SlimBook = () => {
           {/* Папір */}
           <div className="row m-3 p-2">
             <div className="col-sm-12 col-md-7">
-              <img
-                src="images/services/photo/photobooks/BamBook/5.jpg"
-                alt="Папір"
-              />
+            <Zoom {...zoomInProperties}>
+                {img_slider3.map((each, index) => (
+                  <div key={index} style={{ width: "100%" }}>
+                    <img
+                      style={{ objectFit: "cover", width: "100%" }}
+                      src={each}
+                    />
+                  </div>
+                ))}
+              </Zoom>
             </div>
             <div className="col-sm-12 col-md-5 px-3">
               <h3 className="text-center">Папір</h3>
               <p>
-                Розвороти Bambook-ів друкуються на крейдованому папері
-                граматурою 200 г/м2, з використанням цифрових поліграфічних
-                машин XEROX. Папір, що використовується, має підвищену ступінь
-                білизни та покращені характеристики кольоропередачі. При
-                поліграфічному друці згин посередині розвороту призводить до
-                появи білої смуги, котра є об'єктивною особливістю цифрового
-                друку. Ми не радимо використовувати у ділянці згину темні фони.
-                Розміщення облич та сюжетно важливих елементів також не бажане у
-                ділянці згину.
+                Розвороти Ваших Слімбуків ми друкуємо на фотопапері Fujicolor 
+                Crystal Archive Lustre. Даний тип паперу використовується за 
+                замовчуванням для друку фотокниг slimbook та photobook. Папір 
+                володіє специфічним рельєфом та характерною кольоропередачею. 
+                Ми не використовуємо традиційний матовий папір (Matte), тим не 
+                менш, при оформленні замовлення, коли у меню вибору паперу ви 
+                побачите опцію «матовий» — знайте, що це «люстра».
               </p>
             </div>
           </div>
 
-          {/* Ламінація — Ілюмінація */}
+          {/* Фотообкладинка */}
           <div className="row m-3 p-2">
             <div className="col-sm-12 col-md-5 px-3">
-              <h3 className="text-center">Ламінація — Ілюмінація</h3>
+              <h3 className="text-center">Фотообкладинка</h3>
               <p>
-                Це те, що ми називаємо БамБук Ілюмінат. З одного боку, це той
-                самий БамБук, з іншого — абсолютно інша по якості та сприйняттю
-                фотокнига. Багатомісячні експерименти та пошук правильних
-                цифрових матеріалів дозволяють з впевненістю сказати, що це
-                абсолютно новий вид поліграфічної фотокниги. Глянцеве покриття
-                робить кольори насиченими та глибокоми. Блиск — надає
-                презентабельності та розкоші. Виберіть при замовленні Бамбука
-                опцію «ламінація» і переконайтесь на власні очі — це абсолютно
-                нова фотокнига.
+                Базова обкладинка усіх наших фотокниг. Як і у Printbook вона 
+                тверда, надрукована цифровим способом та покрита глянцевою ламінацією.
               </p>
             </div>
             <div className="col-sm-12 col-md-7">
@@ -157,7 +175,7 @@ const SlimBook = () => {
             </div>
           </div>
 
-          {/* Обкладинка */}
+          {/* Обкладинка з шкірзаму */}
           <div className="row m-3 p-2">
             <div className="col-sm-12 col-md-7">
               <Zoom {...zoomInProperties}>
@@ -172,20 +190,68 @@ const SlimBook = () => {
               </Zoom>
             </div>
             <div className="col-sm-12 col-md-5 px-3">
-              <h3 className="text-center">Обкладинка</h3>
+              <h3 className="text-center">Обкладинка з шкірзаму</h3>
               <p>
-                Будь-яка фотокнига починається з обкладинки. У базовій
-                комплектації всі фотокниги оздоблюють твердою ламінованою
-                фотообкладинкою, але її можна оформити і такими матеріалами як:
-                шкірзам, тканина, велюр.
-                <br />
-                На усіх матеріалах обкладинок можна використати додаткове
-                оздоблення, щоб Ваша книга стала ще більш індивідуальною.
+                Більше життя, більше кольору! Обирайте ту обкладинку, яка підходить 
+                під тональність та настрій Вашої фотокниги. Високоякісний 
+                італійський шкірозамінник залишить приємні тактильні відчуття,
+                 а Ваша совість залишиться спокійною, бо жодне звірятко не було вбите.
               </p>
             </div>
           </div>
-        </div>
 
+          {/* Шкірзам з фотовставкою */}
+          <div className="row m-3 p-2">
+            <div className="col-sm-12 col-md-5 px-3">
+              <h3 className="text-center">Шкірзам з фотовставкою</h3>
+              <p>
+                Чудове рішення для того, щоб прикрасити Вашу книгу. Замовляйте 
+                шкірзамобкладинку з фотовставкою із акрилового скла, і Ваша 
+                фотокнига стане унікальною та неповторною.
+              </p>
+            </div>
+            <div className="col-sm-12 col-md-7">
+              <Zoom {...zoomInProperties}>
+                {img_slider6.map((each, index) => (
+                  <div key={index} style={{ width: "100%" }}>
+                    <img
+                      style={{ objectFit: "cover", width: "100%" }}
+                      src={each}
+                    />
+                  </div>
+                ))}
+              </Zoom>
+            </div>
+          </div>
+
+          {/* Тканина */}
+          <div className="row m-3 p-2">
+            <div className="col-sm-12 col-md-7">
+              <Zoom {...zoomInProperties}>
+                {img_slider7.map((each, index) => (
+                  <div key={index} style={{ width: "100%" }}>
+                    <img
+                      style={{ objectFit: "cover", width: "100%" }}
+                      src={each}
+                    />
+                  </div>
+                ))}
+              </Zoom>
+            </div>
+            <div className="col-sm-12 col-md-5 px-3">
+              <h3 className="text-center">Тканина</h3>
+              <p>
+                Обкладинка зі спеціальної тканини від відомого голландського 
+                виробника палітурних матеріалів. Фотокнига в такій обкладинці
+                 виглядає більш «книжно» і злегка нагадує традиційну палітурку,
+                  як в старих книгах. Обирайте, яка саме вам до душі: фактурна 
+                  серія «Льон», чи яскраві та блискучі кольори обкладинок «Люкс».
+              </p>
+            </div>
+          </div>
+
+        </div>
+        
         <div
           className="col py-3 mt-3 mb-3"
           style={{ backgroundColor: "#CDE8F2" }}
