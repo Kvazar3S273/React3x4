08738afdac1f475153da0f42de9e04f217379fd3
Kvazar3S273 <ba2h007@gmail.com>
2022-08-17 23:47:16 +0300

    edit availables photobooks

--- src/components/pages/photo/photobooks/availableBooks.js
+++ src/components/pages/photo/photobooks/availableBooks.js
@@ -1,204 +1,156 @@
 export function Available30x30() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/30x30.png"
-            className="available-format-img"
-            alt="30x30"
-          />
-          <h6 className="text-center text-dark">30x30</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/30x30.png"
+        className="available-format-img "
+        alt="30x30"
+      />
+      <h6 className="text-center text-dark">30x30</h6>
+    </div>
   );
 }
 
 export function Available24x32() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/24x32.png"
-            className="available-format-img"
-            alt="24x32"
-          />
-          <h6 className="text-center text-dark">24x32</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/24x32.png"
+        className="available-format-img"
+        alt="24x32"
+      />
+      <h6 className="text-center text-dark">24x32</h6>
+    </div>
   );
 }
 
 export function Available20x30() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/20x30.png"
-            className="available-format-img"
-            alt="20x30"
-          />
-          <h6 className="text-center text-dark">20x30</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/20x30.png"
+        className="available-format-img"
+        alt="20x30"
+      />
+      <h6 className="text-center text-dark">20x30</h6>
+    </div>
   );
 }
 
 export function Available19x19() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/19x19.png"
-            className="available-format-img"
-            alt="19x19"
-          />
-          <h6 className="text-center text-dark">19x19</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/19x19.png"
+        className="available-format-img"
+        alt="19x19"
+      />
+      <h6 className="text-center text-dark">19x19</h6>
+    </div>
   );
 }
 
 export function Available23x23() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/23x23.png"
-            className="available-format-img"
-            alt="23x23"
-          />
-          <h6 className="text-center text-dark">23x23</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/23x23.png"
+        className="available-format-img"
+        alt="23x23"
+      />
+      <h6 className="text-center text-dark">23x23</h6>
+    </div>
   );
 }
 
 export function Available23x18() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/23x18.png"
-            className="available-format-img"
-            alt="23x18"
-          />
-          <h6 className="text-center text-dark">23x18</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/23x18.png"
+        className="available-format-img"
+        alt="23x18"
+      />
+      <h6 className="text-center text-dark">23x18</h6>
+    </div>
   );
 }
 
 export function Available18x24() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/18x24.png"
-            className="available-format-img"
-            alt="18x24"
-          />
-          <h6 className="text-center text-dark">18x24</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/18x24.png"
+        className="available-format-img"
+        alt="18x24"
+      />
+      <h6 className="text-center text-dark">18x24</h6>
+    </div>
   );
 }
 
 export function Available29x19() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/29x19.png"
-            className="available-format-img"
-            alt="29x19"
-          />
-          <h6 className="text-center text-dark">29x19</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/29x19.png"
+        className="available-format-img"
+        alt="29x19"
+      />
+      <h6 className="text-center text-dark">29x19</h6>
+    </div>
   );
 }
 
 export function Available30x20() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/30x20.png"
-            className="available-format-img"
-            alt="30x20"
-          />
-          <h6 className="text-center text-dark">30x20</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/30x20.png"
+        className="available-format-img"
+        alt="30x20"
+      />
+      <h6 className="text-center text-dark">30x20</h6>
+    </div>
   );
 }
 
 export function Available20x20() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/20x20.png"
-            className="available-format-img"
-            alt="20x20"
-          />
-          <h6 className="text-center text-dark">20x20</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/20x20.png"
+        className="available-format-img"
+        alt="20x20"
+      />
+      <h6 className="text-center text-dark">20x20</h6>
+    </div>
   );
 }
 
 export function Available15x20() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/15x20.png"
-            className="available-format-img"
-            alt="15x20"
-          />
-          <h6 className="text-center text-dark">15x20</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/15x20.png"
+        className="available-format-img"
+        alt="15x20"
+      />
+      <h6 className="text-center text-dark">15x20</h6>
+    </div>
   );
 }
 
 export function Available20x15() {
   return (
-    <>
-      <li style={{ listStyleType: "none" }}>
-        <div>
-          <img
-            src="/images/services/photo/photobooks/formats/20x15.png"
-            className="available-format-img"
-            alt="20x15"
-          />
-          <h6 className="text-center text-dark">20x15</h6>
-        </div>
-      </li>
-    </>
+    <div className="text-center">
+      <img
+        src="/images/services/photo/photobooks/formats/20x15.png"
+        className="available-format-img"
+        alt="20x15"
+      />
+      <h6 className="text-center text-dark">20x15</h6>
+    </div>
   );
 }
 
@@ -211,15 +163,14 @@ export function AvailableBamBooks() {
           Пропонуємо Вам наступні формати фотокниг Bambook:
         </p>
 
-        <ul className="list-group list-group-horizontal justify-content-center">
-            {Available30x30()}
-            {Available24x32()}
-            {Available20x30()}
-            {Available19x19()}
-            {Available23x23()}
-            {Available23x18()}
-        </ul>
-
+        <div className="row">
+          <div className="col">{Available30x30()}</div>
+          <div className="col">{Available24x32()}</div>
+          <div className="col">{Available20x30()}</div>
+          <div className="col">{Available19x19()}</div>
+          <div className="col">{Available23x23()}</div>
+          <div className="col">{Available23x18()}</div>
+        </div>
       </div>
     </>
   );
@@ -234,14 +185,15 @@ export function AvailableSlimBooks() {
           Пропонуємо Вам наступні формати фотокниг Slimbook:
         </p>
 
-        <ul className="list-group list-group-horizontal justify-content-center">
-          {Available30x30()}
-          {Available20x30()}
-          {Available18x24()}
-          {Available23x23()}
-          {Available29x19()}
-          {Available19x19()}
-        </ul>
+        <div className="row">
+          <div className="col">{Available30x30()}</div>
+          <div className="col">{Available20x30()}</div>
+          <div className="col">{Available18x24()}</div>
+          <div className="col">{Available23x23()}</div>
+          <div className="col">{Available29x19()}</div>
+          <div className="col">{Available19x19()}</div>
+        </div>
+        
       </div>
     </>
   );
@@ -256,13 +208,14 @@ export function AvailableUniBooks() {
           Пропонуємо Вам наступні формати фотокниг Unibook:
         </p>
 
-        <ul className="list-group list-group-horizontal justify-content-center">
-          {Available30x30()}
-          {Available20x30()}
-          {Available23x23()}
-          {Available29x19()}
-          {Available19x19()}
-        </ul>
+        <div className="row">
+          <div className="col">{Available30x30()}</div>
+          <div className="col">{Available20x30()}</div>
+          <div className="col">{Available23x23()}</div>
+          <div className="col">{Available29x19()}</div>
+          <div className="col">{Available19x19()}</div>
+        </div>
+
       </div>
     </>
   );
@@ -277,16 +230,17 @@ export function AvailablePrintBooks() {
           Пропонуємо Вам наступні формати фотокниг Printbook:
         </p>
 
-        <ul className="list-group list-group-horizontal justify-content-center">
-          {Available23x23()}
-          {Available24x32()}
-          {Available30x30()}
-          {Available20x30()}
-          {Available30x20()}
-          {Available20x20()}
-          {Available15x20()}
-          {Available20x15()}
-        </ul>
+        <div className="row">
+          <div className="col">{Available23x23()}</div>
+          <div className="col">{Available24x32()}</div>
+          <div className="col">{Available30x30()}</div>
+          <div className="col">{Available20x30()}</div>
+          <div className="col">{Available30x20()}</div>
+          <div className="col">{Available20x20()}</div>
+          <div className="col">{Available15x20()}</div>
+          <div className="col">{Available20x15()}</div>
+        </div>
+
       </div>
     </>
   );
@@ -301,14 +255,15 @@ export function AvailablePhotoBooks() {
           Пропонуємо Вам наступні формати фотокниг Photobook:
         </p>
 
-        <ul className="list-group list-group-horizontal justify-content-center">
-          {Available30x30()}
-          {Available20x30()}
-          {Available18x24()}
-          {Available23x23()}
-          {Available29x19()}
-          {Available19x19()}
-        </ul>
+        <div className="row">
+          <div className="col">{Available30x30()}</div>
+          <div className="col">{Available20x30()}</div>
+          <div className="col">{Available18x24()}</div>
+          <div className="col">{Available23x23()}</div>
+          <div className="col">{Available29x19()}</div>
+          <div className="col">{Available19x19()}</div>
+        </div>
+
       </div>
     </>
   );
