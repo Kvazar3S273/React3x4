c19907c2554198e0bc3bc38039945a549143ff3c
Kvazar3S273 <ba2h007@gmail.com>
2022-08-27 00:22:40 +0300

    edit http common for publish

--- src/http_common.js
+++ src/http_common.js
@@ -2,8 +2,8 @@ import axios from "axios";
 
 export const urlBackend = 'https://hologram-it.tk/';
 export default axios.create({
-    // baseURL: `${urlBackend}`,
-    baseURL: "http://localhost:5000/",
+    baseURL: `${urlBackend}`,
+    // baseURL: "http://localhost:5000/",
     headers:{
         "Content-type":"application/json"
     }
