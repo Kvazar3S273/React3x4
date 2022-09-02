dd285a140aebc8e28f7213c556187828ec512332
Kvazar3S273 <ba2h007@gmail.com>
2022-09-02 17:27:14 +0300

    add modal to price up

--- src/components/pages/comp/laminate/index.js
+++ src/components/pages/comp/laminate/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 const Laminate = () => {
+
   return (
     <div className="row mt-3 mb-3">
       <div className="col py-3" style={{ backgroundColor: "#e0e3e5" }}>
@@ -52,6 +53,48 @@ const Laminate = () => {
                 </tr>
               </tbody>
             </table>
+            <div className="row">
+              <h3 className="text-center">Збільшити ціну на %</h3>
+                <div className="col">Збільшити ціну на</div>
+              <div className="col">
+              <input
+                id="percentValue"
+                style={{
+                  width: "100px",
+                  height: "35px",
+                  textAlign: "center",
+                }}
+              />
+              %
+              </div>
+              <div className="col">
+              <button 
+              type="button" 
+              class="btn btn-primary" 
+              data-bs-toggle="modal" 
+              data-bs-target="#exampleModal">Збільшити</button>
+              </div>
+
+                {/* Modal */}
+              <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                  <div class="modal-content">
+                    <div class="modal-header">
+                      <h5 class="modal-title" id="exampleModalLabel">Попередження</h5>
+                      <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body">
+                      Ви справді плануєте збільшити ціну на ...%?
+                    </div>
+                    <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Ні</button>
+                      <button type="button" class="btn btn-primary">Так, збільшити</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+            </div>
           </div>
         </div>
       </div>
