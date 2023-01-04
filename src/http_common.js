d5456cb15e77a375c3c2ebe864d81ab2f57b2df1
Kvazar3S273 <ba2h007@gmail.com>
2023-01-04 23:45:16 +0200

    edit http common

--- src/http_common.js
+++ src/http_common.js
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-export const urlBackend = 'https://hologram-it.tk/';
+export const urlBackend = 'https://3x4.com.ua/';
 export default axios.create({
-    // baseURL: `${urlBackend}`,
-    baseURL: "http://localhost:5000/",
+    baseURL: `${urlBackend}`,
+
+    // baseURL: "http://localhost:5000/",
     headers:{
         "Content-type":"application/json"
     }
