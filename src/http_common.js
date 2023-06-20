d63f849ca4f59e8baa30823a8ebc526bcd228176
Kvazar3S273 <ba2h007@gmail.com>
2023-06-20 22:16:29 +0300

    edit http_common

--- src/http_common.js
+++ src/http_common.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 export const urlBackend = 'https://3x4.com.ua/';
 export default axios.create({
-    // baseURL: `${urlBackend}`,
+    baseURL: `${urlBackend}`,
 
-    baseURL: "http://localhost:5000/",
+    // baseURL: "http://localhost:5000/",
     headers:{
         "Content-type":"application/json"
     }
